6fabccc16a1115618c89b77843e1b3e8779b2a9e
ahmdaeyz <ahmdaeyz@gmail.com>
2020-06-03 00:57:17 +0200

    Add method initializeSingletons

--- app/src/main/java/dev/ahmdaeyz/guardianscope/GuardianScopeApplication.java
+++ app/src/main/java/dev/ahmdaeyz/guardianscope/GuardianScopeApplication.java
@@ -4,11 +4,21 @@ import android.app.Application;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
+import dev.ahmdaeyz.guardianscope.data.network.BasicNetworkService;
+import dev.ahmdaeyz.guardianscope.data.network.NetworkService;
+import dev.ahmdaeyz.guardianscope.data.repository.ArticlesRepositoryImpl;
+
 public class GuardianScopeApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         AndroidThreeTen.init(this);
+        initializeSingletons();
+    }
+
+    void initializeSingletons() {
+        NetworkService networkService = new BasicNetworkService();
+        ArticlesRepositoryImpl.initRepository(networkService);
     }
 }
