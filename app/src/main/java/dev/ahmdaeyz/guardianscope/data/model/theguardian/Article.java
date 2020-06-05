13ae1e4dc62011a31878df85ede180f9abccee00
ahmdaeyz <ahmdaeyz@gmail.com>
2020-06-06 01:35:20 +0200

    Make the object parcelable and add isBookmarked and isLiked flags

--- app/src/main/java/dev/ahmdaeyz/guardianscope/data/model/theguardian/Article.java
+++ app/src/main/java/dev/ahmdaeyz/guardianscope/data/model/theguardian/Article.java
@@ -1,14 +1,11 @@
 package dev.ahmdaeyz.guardianscope.data.model.theguardian;
 
+import org.parceler.Parcel;
+import org.parceler.ParcelConstructor;
+
+@Parcel
 public class Article {
-    public Article(String id, String sectionName, String webPublicationDate, String webTitle, String webUrl, Fields fields) {
-        this.id = id;
-        this.sectionName = sectionName;
-        this.webPublicationDate = webPublicationDate;
-        this.webTitle = webTitle;
-        this.webUrl = webUrl;
-        this.fields = fields;
-    }
+    private boolean isLiked;
 
     private String id;
     private String sectionName;
@@ -16,7 +13,19 @@ public class Article {
     private String webTitle;
     private String webUrl;
     private Fields fields;
+    private boolean isBookmarked;
 
+    @ParcelConstructor
+    public Article(String id, String sectionName, String webPublicationDate, String webTitle, String webUrl, Fields fields) {
+        this.id = id;
+        this.sectionName = sectionName;
+        this.webPublicationDate = webPublicationDate;
+        this.webTitle = webTitle;
+        this.webUrl = webUrl;
+        this.fields = fields;
+        this.isLiked = false;
+        this.isBookmarked = false;
+    }
 
     public String getId() {
         return id;
@@ -33,6 +42,7 @@ public class Article {
     public String getWebTitle() {
         return webTitle;
     }
+
     public String getWebUrl() {
         return webUrl;
     }
@@ -44,10 +54,26 @@ public class Article {
     @Override
     public String toString() {
         return "{id: " + id +
-                " , sectionName: "+sectionName+
-                " , webPublicationDate: "+webPublicationDate+
-                " , webTitle: "+webTitle+
-                " , webUrl: "+webUrl+
-                " , Fields: "+fields.toString()+"}";
+                " , sectionName: " + sectionName +
+                " , webPublicationDate: " + webPublicationDate +
+                " , webTitle: " + webTitle +
+                " , webUrl: " + webUrl +
+                " , Fields: " + fields.toString() + "}";
+    }
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setLiked(boolean liked) {
+        isLiked = liked;
+    }
+
+    public boolean isBookmarked() {
+        return isBookmarked;
+    }
+
+    public void setBookmarked(boolean bookmarked) {
+        isBookmarked = bookmarked;
     }
 }
