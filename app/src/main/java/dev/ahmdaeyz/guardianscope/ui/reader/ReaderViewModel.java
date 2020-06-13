07ab5dd286bd8c11f9f4e6fc492674c6aead0a1d
ahmdaeyz <ahmdaeyz@gmail.com>
2020-06-13 14:40:48 +0200

    Add apiUrl to fetch article based on its apiUrl based to the viewModel by the fragment

--- app/src/main/java/dev/ahmdaeyz/guardianscope/ui/reader/ReaderViewModel.java
+++ app/src/main/java/dev/ahmdaeyz/guardianscope/ui/reader/ReaderViewModel.java
@@ -2,6 +2,8 @@ package dev.ahmdaeyz.guardianscope.ui.reader;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import dev.ahmdaeyz.guardianscope.data.model.theguardian.Article;
@@ -11,28 +13,43 @@ import io.reactivex.disposables.CompositeDisposable;
 class ReaderViewModel extends ViewModel {
     final ArticlesRepository repository;
     final CompositeDisposable disposables = new CompositeDisposable();
+    private MutableLiveData<Article> _article = new MutableLiveData<>();
+    LiveData<Article> article = _article;
 
     public ReaderViewModel(ArticlesRepository repository) {
         this.repository = repository;
     }
 
-    public void favouriteArticle(Article article) {
+    public void favouriteArticle() {
         disposables.add(
-                repository.favouriteArticle(article)
+                repository.favouriteArticle(article.getValue())
                         .subscribe(() -> {
                         }, (throwable) -> {
                             Log.e("FavouriteArticle", "Failed");
                         }));
     }
 
-    public void bookmarkArticle(Article article) {
+    public void bookmarkArticle() {
         disposables.add(
-                repository.bookMarkArticle(article)
+                repository.bookMarkArticle(article.getValue())
                         .subscribe(() -> {
                         }, (throwable) -> {
                             Log.e("BookmarkArticle", "Failed");
                         }));
     }
 
+    public void apiUrlIs(String apiUrl) {
+        disposables.add(
+                repository.getArticle(apiUrl)
+                        .subscribe(
+                                (article) -> {
+                                    _article.postValue(article);
+                                },
+                                (throwable) -> {
+                                    // handle error
+                                }
+                        )
+        );
+    }
 
 }
